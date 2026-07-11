90adad46cbb8aa9cc41a147ea8be8ce80892af2d
Jiya <jiya@Jiyas-MacBook-Air-3.local>
2026-07-12 00:13:25 +0530

    Updated HeroSection and Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
-import Hero from "./components/HeroSection";
-import PredictForm from "./components/PredictForm";
-import ResultCard from "./components/ResultCard";
-import AnalysisSection from "./components/AnalysisSection";
+import HeroSection from "./components/HeroSection";
+// import PredictForm from "./components/PredictForm";
+// import ResultCard from "./components/ResultCard";
+// import AnalysisSection from "./components/AnalysisSection";
 
 export default function App() {
   const [result, setResult] = useState(null);
@@ -13,9 +13,9 @@ export default function App() {
     <div className="min-h-screen bg-gray-950 text-white">
       <Navbar />
       <HeroSection />
-      <PredictForm setResult={setResult} setLoading={setLaoding} loading={loading} />
+      {/* <PredictForm setResult={setResult} setLoading={setLaoding} loading={loading} />
       {result && <ResultCard result={result} />}
-      {result && <AnalysisSection result={result} />}
+      {result && <AnalysisSection result={result} />} */}
     </div>
   )
 }
